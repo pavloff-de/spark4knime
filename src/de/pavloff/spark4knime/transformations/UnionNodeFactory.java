9739a54b1343afc17c821c139ce43da839046c4c
pavloff-de <oleg@pavloff.de>
2015-05-28 20:33:55 +0200

    Node for union of two RDD's

---
+++ src/de/pavloff/spark4knime/transformations/UnionNodeFactory.java
@@ -0,0 +1,58 @@
+package de.pavloff.spark4knime.transformations;
+
+import org.knime.core.node.NodeDialogPane;
+import org.knime.core.node.NodeFactory;
+import org.knime.core.node.NodeView;
+
+/**
+ * <code>NodeFactory</code> for the "Union" Node.
+ * Creates a new RDD that contains the union of two RDD's
+ *
+ * @author Oleg Pavlov
+ */
+public class UnionNodeFactory 
+        extends NodeFactory<UnionNodeModel> {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public UnionNodeModel createNodeModel() {
+        return new UnionNodeModel();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getNrNodeViews() {
+        return 1;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public NodeView<UnionNodeModel> createNodeView(final int viewIndex,
+            final UnionNodeModel nodeModel) {
+        return new UnionNodeView(nodeModel);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean hasDialog() {
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public NodeDialogPane createNodeDialogPane() {
+        return new UnionNodeDialog();
+    }
+
+}
+
---
+++ src/de/pavloff/spark4knime/transformations/UnionNodeModel.java
@@ -0,0 +1,159 @@
+package de.pavloff.spark4knime.transformations;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaRDDLike;
+import org.knime.core.data.DataTableSpec;
+import org.knime.core.node.BufferedDataTable;
+import org.knime.core.node.CanceledExecutionException;
+import org.knime.core.node.ExecutionContext;
+import org.knime.core.node.ExecutionMonitor;
+import org.knime.core.node.InvalidSettingsException;
+import org.knime.core.node.NodeLogger;
+import org.knime.core.node.NodeModel;
+import org.knime.core.node.NodeSettingsRO;
+import org.knime.core.node.NodeSettingsWO;
+
+import de.pavloff.spark4knime.RddTable;
+
+/**
+ * This is the model implementation of Union. Creates a new RDD that contains
+ * the union of two RDD's
+ * 
+ * @author Oleg Pavlov
+ */
+public class UnionNodeModel extends NodeModel {
+
+	// the logger instance
+	private static final NodeLogger logger = NodeLogger
+			.getLogger(UnionNodeModel.class);
+
+	/**
+	 * Constructor for the node model.
+	 */
+	protected UnionNodeModel() {
+		// input: two BufferedDataTables with JavaRDD
+		// output: BufferedDataTable with union of JavaRDD's
+		super(2, 1);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@Override
+	protected BufferedDataTable[] execute(final BufferedDataTable[] inData,
+			final ExecutionContext exec) throws Exception {
+		if (inData.length != 2) {
+			throw new IndexOutOfBoundsException(
+					"inData should contain two tables each with JavaRDDLike");
+		}
+		
+		JavaRDDLike rdd;
+		if (RddTable.isPairRDD(inData[0])) {
+			if (RddTable.isPairRDD(inData[1])) {
+				rdd = ((JavaPairRDD) RddTable.getRDD(inData[0])).union((JavaPairRDD) RddTable.getRDD(inData[1]));
+				return new BufferedDataTable[] { RddTable.setRDD(exec, rdd, true) };
+			} else {
+				throw new IllegalArgumentException("RDD's must be of same type");
+			}
+		} else {
+			if (RddTable.isPairRDD(inData[1])) {
+				throw new IllegalArgumentException("RDD's must be of same type");
+			} else {
+				rdd = ((JavaRDD) RddTable.getRDD(inData[0])).union((JavaRDD) RddTable.getRDD(inData[1]));
+				return new BufferedDataTable[] { RddTable.setRDD(exec, rdd, false) };
+			}
+		}
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void reset() {
+		// TODO Code executed on reset.
+		// Models build during execute are cleared here.
+		// Also data handled in load/saveInternals will be erased here.
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected DataTableSpec[] configure(final DataTableSpec[] inSpecs)
+			throws InvalidSettingsException {
+
+		// TODO: check if user settings are available, fit to the incoming
+		// table structure, and the incoming types are feasible for the node
+		// to execute. If the node can execute in its current state return
+		// the spec of its output data table(s) (if you can, otherwise an array
+		// with null elements), or throw an exception with a useful user message
+
+		return new DataTableSpec[] { null };
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void saveSettingsTo(final NodeSettingsWO settings) {
+
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void loadValidatedSettingsFrom(final NodeSettingsRO settings)
+			throws InvalidSettingsException {
+
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void validateSettings(final NodeSettingsRO settings)
+			throws InvalidSettingsException {
+
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void loadInternals(final File internDir,
+			final ExecutionMonitor exec) throws IOException,
+			CanceledExecutionException {
+
+		// TODO load internal data.
+		// Everything handed to output ports is loaded automatically (data
+		// returned by the execute method, models loaded in loadModelContent,
+		// and user settings set through loadSettingsFrom - is all taken care
+		// of). Load here only the other internals that need to be restored
+		// (e.g. data used by the views).
+
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void saveInternals(final File internDir,
+			final ExecutionMonitor exec) throws IOException,
+			CanceledExecutionException {
+
+		// TODO save internal models.
+		// Everything written to output ports is saved automatically (data
+		// returned by the execute method, models saved in the saveModelContent,
+		// and user settings saved through saveSettingsTo - is all taken care
+		// of). Save here only the other internals that need to be preserved
+		// (e.g. data used by the views).
+
+	}
+
+}
