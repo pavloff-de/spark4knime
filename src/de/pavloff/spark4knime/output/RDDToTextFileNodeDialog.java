305712cbe26933966bc811b38eb1850cc778c64b
pavloff-de <oleg@pavloff.de>
2015-08-05 20:49:03 +0200

    Node for saving RDD as text file

---
+++ src/de/pavloff/spark4knime/output/RDDToTextFileNodeDialog.java
@@ -0,0 +1,42 @@
+package de.pavloff.spark4knime.output;
+
+import javax.swing.JFileChooser;
+
+import org.knime.core.node.defaultnodesettings.DefaultNodeSettingsPane;
+import org.knime.core.node.defaultnodesettings.DialogComponentBoolean;
+import org.knime.core.node.defaultnodesettings.DialogComponentFileChooser;
+import org.knime.core.node.defaultnodesettings.SettingsModelBoolean;
+import org.knime.core.node.defaultnodesettings.SettingsModelString;
+
+/**
+ * <code>NodeDialog</code> for the "RDDToTextFile" Node. Save RDD to Text File
+ * 
+ * This node dialog derives from {@link DefaultNodeSettingsPane} which allows
+ * creation of a simple dialog with standard components. If you need a more
+ * complex dialog please derive directly from
+ * {@link org.knime.core.node.NodeDialogPane}.
+ * 
+ * @author Oleg Pavlov
+ */
+public class RDDToTextFileNodeDialog extends DefaultNodeSettingsPane {
+
+	/**
+	 * New pane for configuring RDDToTextFile node dialog. This is just a
+	 * suggestion to demonstrate possible default dialog components.
+	 */
+	protected RDDToTextFileNodeDialog() {
+		super();
+		
+		// text file
+		addDialogComponent(new DialogComponentFileChooser(
+				new SettingsModelString(RDDToTextFileNodeModel.CFGKEY_PATH,
+						RDDToTextFileNodeModel.DEFAULT_PATH),
+				"spark.file.writer", JFileChooser.OPEN_DIALOG, true));
+		
+		// overwrite
+		addDialogComponent(new DialogComponentBoolean(new SettingsModelBoolean(
+				RDDToTextFileNodeModel.CFGKEY_OVERWRITE,
+				RDDToTextFileNodeModel.DEFAULT_OVERWRITE), "Overwrite if the directory already exists ?"));
+
+	}
+}
---
+++ src/de/pavloff/spark4knime/output/RDDToTextFileNodeFactory.java
@@ -0,0 +1,58 @@
+package de.pavloff.spark4knime.output;
+
+import org.knime.core.node.NodeDialogPane;
+import org.knime.core.node.NodeFactory;
+import org.knime.core.node.NodeView;
+
+/**
+ * <code>NodeFactory</code> for the "RDDToTextFile" Node.
+ * Save RDD to Text File
+ *
+ * @author Oleg Pavlov
+ */
+public class RDDToTextFileNodeFactory 
+        extends NodeFactory<RDDToTextFileNodeModel> {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public RDDToTextFileNodeModel createNodeModel() {
+        return new RDDToTextFileNodeModel();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getNrNodeViews() {
+        return 0;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public NodeView<RDDToTextFileNodeModel> createNodeView(final int viewIndex,
+            final RDDToTextFileNodeModel nodeModel) {
+        return new RDDToTextFileNodeView(nodeModel);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean hasDialog() {
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public NodeDialogPane createNodeDialogPane() {
+        return new RDDToTextFileNodeDialog();
+    }
+
+}
+
